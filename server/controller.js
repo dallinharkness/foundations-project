cabb281aebf543d1700407ee1779e4f60d8e07cf
Dallin Harkness <dalhark99@gmail.com>
2021-12-23 10:43:07 -0700

    env

---
+++ server/controller.js
@@ -0,0 +1,13 @@
+require('dotenv').config()
+
+const{CONNECTION_STRING}=process.env
+
+const Sequelize = require('sequelize')
+const sequelize = new Sequelize(CONNECTION_STRING, {
+    dialect: 'postgres',
+    dialectOptions: {
+        ssl: {
+            rejectUnauthorized: false
+        }
+    }
+})
