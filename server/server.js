1b7ff56c5f4b5e31e0e4a690a22d6e9784027571
Dallin Harkness <dalhark99@gmail.com>
2021-12-23 10:55:33 -0700

    deployed

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
 const {SERVER_PORT} = process.env
-
+const path = require('path')
 const{
     getUser,
     getScores,
@@ -15,10 +15,13 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
+app.get('/',function(req,res) {
+    res.sendFile(path.join(__dirname, '../../home.html'))
+  })
 
-app.get('/api/users', getUser)
-app.post('/api/newuser', newUser)
-app.put('/api/newscores', getScores)
+// app.get('/api/users', getUser)
+// app.post('/api/newuser', newUser)
+// app.put('/api/newscores', getScores)
 
 
 app.listen(SERVER_PORT, () => console.log(`running on ${SERVER_PORT}`))
